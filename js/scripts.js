7837b280428ef4e3211153d776a3e19f3bb0b1fc
Felipe <f.delaguerra@gmail.com>
2019-04-24 12:59:18 -0400

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+const pic1 = document.getElementById('pic1')
+
+
+function showF1() {
+    pic1.style.display = 'block';
+}
+
+pic1.addEventListener('click', showF1) {
+
+}
+
+
+/*
+let hide = document.querySelector('#answer');
+let caption = document.querySelector('#eggcap');
+document.addEventListener('DOMContentLoaded', () => {
+
+   hide.style.display = 'none';
+   caption.style.display = 'none';
+   final.style.display = 'none';
+
+});
+
+let question = document.querySelector('#eggpic');
+
+
+
+
+
+*/
